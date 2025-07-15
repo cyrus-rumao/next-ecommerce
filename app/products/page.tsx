f5272ddbcaba633bd381976fc6ae2e3e209c847f
Cyrus <cyrus.rumao26@gmail.com>
2025-07-16 00:37:37 +0530

    initial Commit

---
+++ app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function productsPage() {
+    return (
+        <div>
+        <h1>Products Page</h1>
+        <p>This is where the products will be listed.</p>
+        </div>
+    );
+}
