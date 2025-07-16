9ad68022ff4cf863677e46a9f9370f758e0d7670
Cyrus <cyrus.rumao26@gmail.com>
2025-07-17 00:10:09 +0530

    fix issues

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,20 +1,18 @@
 import { stripe } from '@/lib/stripe';
 import { ProductDetail } from '@/components/product-detail';
 
-type Props = {
-	params: {
-		id: string;
-	};
-};
+export default async function ProductPage({
+	params,
+}: {
+	params: { id: string };
+}) {
+	const { id } = params;
 
-export default async function ProductPage({ params }: Props) {
-
-  const { id } = await params;
-	const Product = await stripe.products.retrieve(id, {
+	const productRaw = await stripe.products.retrieve(id, {
 		expand: ['default_price'],
 	});
 
-	const product = JSON.parse(JSON.stringify(Product));
+	const product = JSON.parse(JSON.stringify(productRaw));
 
 	return <ProductDetail product={product} />;
 }
